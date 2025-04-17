844828ad73a23002dbd17e7026b361ff7ed963c5
いるかぁぁ <104310722+Dolphin-kun@users.noreply.github.com>
2025-04-17 19:34:06 +0900

    Add files via upload

---
+++ LyricMotionEffect.cs
@@ -0,0 +1,105 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using YukkuriMovieMaker.Commons;
+using YukkuriMovieMaker.Controls;
+using YukkuriMovieMaker.Exo;
+using YukkuriMovieMaker.Player.Video;
+using YukkuriMovieMaker.Plugin.Effects;
+
+namespace LyricMotion
+{
+    [VideoEffect("リリックモーションしながら登場退場", ["登場退場"], ["リリックモーション", "Lyric Motion", "ririkku"],IsAviUtlSupported =false)]
+    internal class LyricMotionEffect : VideoEffectBase
+    {
+        public override string Label
+        {
+            get
+            {
+                if (EffectEnter && !EffectExit)
+                    return "リリックモーションしながら登場";
+                if (!EffectEnter && EffectExit)
+                    return "リリックモーションしながら退場";
+                if (!EffectEnter && !EffectExit)
+                    return "リリックモーションしながら";
+                return "リリックモーションしながら登場退場";
+            }
+        }
+
+        //登場退場
+        [Display(GroupName = "登場退場", Name = "登場時", Description = "アイテムが登場する際にエフェクトを適用する")]
+        [ToggleSlider]
+        public bool EffectEnter
+        {
+            get => effectEnter;
+            set => Set(ref effectEnter, value, nameof(EffectEnter), nameof(Label));
+        }
+        bool effectEnter = true;
+
+        [Display(GroupName = "登場退場", Name = "退場時", Description = "アイテムが退場する際にエフェクトを適用する")]
+        [ToggleSlider]
+        public bool EffectExit
+        {
+            get => effectExit;
+            set => Set(ref effectExit, value, nameof(EffectExit), nameof(Label));
+        }
+        bool effectExit = true;
+
+        [Display(GroupName = "登場退場", Name = "X方向", Description = "X方向")]
+        [ToggleSlider]
+        public bool Direction_X { get => x; set => Set(ref x, value); }
+        bool x = true;
+
+        [Display(GroupName = "登場退場", Name = "Y方向", Description = "Y方向")]
+        [ToggleSlider]
+        public bool Direction_Y { get => y; set => Set(ref y, value); }
+        bool y = true;
+
+        [Display(GroupName = "登場退場", Name = "双方向", Description = "双方向")]
+        [ToggleSlider]
+        public bool Bidirectional { get => bidirectional; set => Set(ref bidirectional, value); }
+        bool bidirectional = true;
+
+        [Display(GroupName = "登場退場", Name = "同一方向", Description = "同一方向")]
+        [ToggleSlider]
+        public bool Same_Direction { get => same_Direction; set => Set(ref same_Direction, value); }
+        bool same_Direction = false;
+
+        [Display(GroupName = "登場退場", Name = "効果時間", Description = "エフェクトが再生される秒数")]
+        [TextBoxSlider("F2", "秒", 0, 0.5)]
+        [DefaultValue(0d)]
+        [Range(0, 99999d)]
+        public double PlayTime { get => time; set => Set(ref time, value); }
+        double time = 0.30;
+
+        [Display(GroupName = "登場退場", Name = "距離", Description = "距離")]
+        [AnimationSlider("F1", "px", -100.0, 100.0)]
+        public Animation Distance { get; } = new Animation(100.0, -99999.0, 99999.0);
+
+        [Display(GroupName = "登場退場", Name = "シード", Description = "シード値")]
+        [AnimationSlider("F0", "", 0, 10)]
+        public Animation Offset { get; } = new Animation(0, 0, 1000);
+
+        [Display(GroupName = "登場退場", Name = "種類", Description = "アニメーションの種類")]
+        [EnumComboBox]
+        public EasingType EasingType { get => easingType; set => Set(ref easingType, value); }
+        EasingType easingType = EasingType.Expo;
+
+        [Display(GroupName = "登場退場", Name = "加減速", Description = "アニメーションの加減速")]
+        [EnumComboBox]
+        public EasingMode EasingMode { get => easingMode; set => Set(ref easingMode, value); }
+        EasingMode easingMode = EasingMode.Out;
+
+
+        public override IEnumerable<string> CreateExoVideoFilters(int keyFrameIndex, ExoOutputDescription exoOutputDescription)
+        {
+            return [];
+        }
+
+        public override IVideoEffectProcessor CreateVideoEffect(IGraphicsDevicesAndContext devices)
+        {
+            return new LyricMotionEffectProcessor(devices, this);
+        }
+
+        protected override IEnumerable<IAnimatable> GetAnimatables() => [Distance, Offset];
+    }
+}
---
+++ LyricMotionEffectProcessor.cs
@@ -0,0 +1,107 @@
+﻿using Vortice.Direct2D1;
+using YukkuriMovieMaker.Commons;
+using YukkuriMovieMaker.Player.Video;
+
+namespace LyricMotion
+{
+    internal partial class LyricMotionEffectProcessor : IVideoEffectProcessor
+    {
+        private readonly IGraphicsDevicesAndContext devices;
+        readonly LyricMotionEffect item;
+        ID2D1Image? input;
+
+        public ID2D1Image Output => input ?? throw new NullReferenceException(nameof(input) + "is null");
+
+        public LyricMotionEffectProcessor(IGraphicsDevicesAndContext devices, LyricMotionEffect item)
+        {
+            this.devices = devices;
+            this.item = item;
+        }
+
+        public DrawDescription Update(EffectDescription effectDescription)
+        {
+            var frame = effectDescription.ItemPosition.Frame;
+            var length = effectDescription.ItemDuration.Frame;
+            var fps = effectDescription.FPS;
+
+            var textIndex = effectDescription.InputIndex;
+            var offset = item.Offset.GetValue(frame, length, fps);
+            var distance = item.Distance.GetValue(frame, length, fps);
+
+            var dx = item.Direction_X;
+            var dy = item.Direction_Y;
+            var bid = item.Bidirectional;
+
+            List<(int xDir, int yDir)> directions = [];
+
+            if (dx)
+            {
+                directions.Add((1, 0));
+                if (bid) directions.Add((-1, 0));
+            }
+            if (dy)
+            {
+                directions.Add((0, 1));
+                if (bid) directions.Add((0, -1));
+            }
+
+            if (directions.Count == 0)
+                return effectDescription.DrawDescription;
+
+            bool isEnter = item.EffectEnter && (double)frame / fps <= item.PlayTime;
+            bool isExit = item.EffectExit && (double)frame / fps >= (double)length / fps - item.PlayTime;
+
+            if (!isEnter && !isExit)
+                return effectDescription.DrawDescription;
+
+            int seed = ((100 + textIndex + (int)offset) * (100 + textIndex)) + textIndex;
+            int rand = new Random(seed % int.MaxValue).Next(directions.Count);
+            var (xDir, yDir) = directions[rand];
+
+
+            double easingRate = 0;
+            if (isEnter)
+            {
+                double rate = frame / item.PlayTime / fps;
+                easingRate = Easing.GetValue(item.EasingType, item.EasingMode, Math.Clamp(rate, 0, 1));
+                easingRate = 1 - easingRate;
+            }
+            if (isExit)
+            {
+                double rate = (length - frame) / item.PlayTime / fps;
+                easingRate = Easing.GetValue(item.EasingType, item.EasingMode, Math.Clamp(rate, 0, 1));
+                easingRate = (1 - easingRate) * (item.Same_Direction ? -1 : 1);
+            }
+
+            double x = xDir * easingRate * distance;
+            double y = yDir * easingRate * distance;
+
+            var drawDesc = effectDescription.DrawDescription;
+            return drawDesc with
+            {
+                Draw = new(
+                    drawDesc.Draw.X + (float)x,
+                    drawDesc.Draw.Y + (float)y,
+                    drawDesc.Draw.Z
+                ),
+            };
+
+        }
+
+
+        public void ClearInput()
+        {
+            input = null;
+        }
+
+        public void SetInput(ID2D1Image? input)
+        {
+            this.input = input;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+
+}
